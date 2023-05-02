96e32ac9a25d58055352488a42c7a86fd24bb5c6
Carlos Rojas <carlosandresrojasvalbuena521@gmail.com>
2023-05-01 21:41:21 -0500

    solve challenges

--- test/game5Test.js
+++ test/game5Test.js
@@ -2,20 +2,27 @@ const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
 
 describe('Game5', function () {
+
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
 
-    return { game };
+    const signer = ethers.provider.getSigner(0);
+
+    return { game, signer };
   }
+
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
+    const wallet = ethers.Wallet.createRandom();
+    console.log(`Your lucky wallet address is: ${wallet.address}`);
 
-    await game.win();
+    await game.connect(wallet.address).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
   });
+
 });
